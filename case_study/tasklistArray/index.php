6fb15d500ef334c02a9ae4ae7209af1540ec10cb
ryoga <sarassoroberto@gmail.com>
2021-03-08 10:24:22 +0100

    Versione HTML del layout

--- case_study/tasklistArray/index.php
+++ case_study/tasklistArray/index.php
@@ -1,1 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/style.css">
+    <title>Task list</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="searchText" >
+        <button type="submit">cerca</button>
+    </form>
+    <ul>
+        <li class="tasklist-item tasklist-item-progress">latte <b>progress</b></li>
+        <li class="tasklist-item tasklist-item-done">uova  <b>done</b></li>
+        <li class="tasklist-item tasklist-item-todo">uova  <b>todo</b></li>
+    </ul>
+</body>
+</html>
+
