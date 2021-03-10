835b5d7c04bf69ce1c83e0c35a4ed434dd2e49ba
ryoga <sarassoroberto@gmail.com>
2021-03-10 11:57:23 +0100

    tollti refusi dell es funzionale

--- case_study/tasklistArray/lib/searchFunctions.php
+++ case_study/tasklistArray/lib/searchFunctions.php
@@ -1,21 +1,4 @@
 <?php
-/**
- * Funzione di ordine superiore funzione che restituisce una funzione
- * Programmazione Funzionale - dichiarativo 
- */
-function _searchText($searchText) {
-    // "Latte" // "php"
-    return function ($taskItem) use ($searchText) {
-            
-            // [0] => "prendere il latte" --> 12 != --> true
-            // [1] => "fare benzina" --> false  --> false !== false
-            // [2] => "latte per il viso" --> 0  !== falso ---> true
-            $result = strpos($taskItem['taskName'] ,$searchText) !== false;
-            return $result;
-    };
-    
-    // return 10 ;
-}
 
 /**
  * Imperativo
