e90bead219428dfb126961743a3185c0618fdaa7
corsidrive <corsidrive@gmail.com>
2021-03-24 16:30:32 +0100

    dopo pausa

---
+++ oop_in_php/tests/class/TaskIsExpiredTest.php
@@ -0,0 +1,20 @@
+<?php
+require './vendor/testTools/testTool.php';
+require './class/Task.php';
+
+$dataset = [
+    ['2021-03-23',true,'task scaduta ieri'],
+    ['2021-03-24',false,'task di oggi'],
+    ['2021-03-25',false,'task che scade domani'],
+];
+
+foreach ($dataset as $testCase) {
+    // $inputDate = $testCase[0]  
+    // $expected = $testCase[1]  
+    // $description = $testCase[2]  
+    list($inputDate,$expected,$description) = $testCase;
+   
+    $task = new Task();
+       
+
+}
